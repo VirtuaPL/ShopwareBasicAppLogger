17fa77af8cdcdd4aa054c6936aa21ca0b72142f2
Mateusz Banasik <m.banasik@wearevirtua.com>
2022-07-26 15:28:01 +0200

    update loggr service

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -4,18 +4,6 @@ declare(strict_types=1);
 
 namespace Virtua\ShopwareBasicAppLoggerBundle\Service;
 
-use App\Repository\ShopwareShopRepository;
-use Virtua\ShopwareAppLoggerBundle\Repository\ShopwareAppLogRepository;
-
 class Logger extends  LoggerAbstract
 {
-    private ShopwareAppLogRepository $logRepository;
-    private ShopwareShopRepository $shopRepository;
-
-    public function __construct(
-        ShopwareAppLogRepository $logRepository,
-        ShopwareShopRepository $shopRepository
-    ) {
-        parent::__construct($logRepository, $shopRepository);
-    }
 }
--- src/Service/LoggerAbstract.php
+++ src/Service/LoggerAbstract.php
@@ -4,42 +4,12 @@ declare(strict_types=1);
 
 namespace Virtua\ShopwareBasicAppLoggerBundle\Service;
 
-use App\Repository\ShopwareShopRepository;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Virtua\ShopwareAppLoggerBundle\Entity\ShopwareAppLog;
-use Virtua\ShopwareAppLoggerBundle\Util\LoggerDataAbstract;
+use Virtua\ShopwareBasicAppLoggerBundle\Util\LoggerDataAbstract;
 
 abstract class LoggerAbstract
 {
-    private ServiceEntityRepository $logRepository;
-    private ServiceEntityRepository $shopRepository;
-
-    public function __construct(
-        ServiceEntityRepository $logRepository,
-        ServiceEntityRepository $shopRepository
-    ) {
-        $this->logRepository = $logRepository;
-        $this->shopRepository = $shopRepository;
-    }
-
-    /**
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
-     */
-    public function log(LoggerDataAbstract $data): void
+    public function log(LoggerDataAbstract $data)
     {
-        $shopEntity = $this->shopRepository->findByShopId($data->getShopId());
-
-        if (!$shopEntity) {
-            return;
-        }
-
-        $logEntity = new ShopwareAppLog();
-        $logEntity->setShopwareShop($shopEntity);
-        $logEntity->setErrorCode($data->getErrorCode());
-        $logEntity->setErrorMessage($data->getErrorMessage());
-        $logEntity->setCreatedAt(new \DateTimeImmutable());
-
-        $this->logRepository->add($logEntity, true);
+        //use your App/Core/Client->updateEntity($logData)
     }
 }
